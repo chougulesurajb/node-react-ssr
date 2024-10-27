bd46126b6765ae33c56dcbaab295d120b7f541c5
chougulesurajb <43784408+chougulesurajb@users.noreply.github.com>
2024-10-27 13:32:04 -0700

    some edits

--- server/index.js
+++ server/index.js
@@ -5,11 +5,53 @@ const App = require("../src/App").default;
 const AppServer = require("../src/AppServer").default;
 const path = require("path");
 const app = express();
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const rootRouter = express.Router();
 
+rootRouter.get("/", (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+});
+
+rootRouter.get("/react", async (req, res, next) => {
+  const content = ReactDOMServer.renderToString(<App />);
+  await fs.readFile(
+    path.resolve(__dirname, "../build", "index.html"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("An error occurred");
+      }
+
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${content}</div>`)
+      );
+    }
+  );
+});
+
+rootRouter.get("/react-ssr", async (req, res, next) => {
+  const content = ReactDOMServer.renderToString(<AppServer />);
+  await fs.readFile(
+    path.resolve(__dirname, "../build", "index.html"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("An error occurred");
+      }
+
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${content}</div>`)
+      );
+    }
+  );
+});
+
 app.use(express.static(path.resolve(__dirname, "../buildserver")));
 app.use(express.static(path.resolve(__dirname, "../build")));
+
 app.use((req, res, next) => {
   if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
     next();
@@ -19,59 +61,8 @@ app.use((req, res, next) => {
   res.header("Pragma", "no-cache");
   next();
 });
+
 app.use(rootRouter);
-
-rootRouter.get("/br", (req, res, next) => {
-  const content = ReactDOMServer.renderToString(<AppServer />);
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-         <head>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="/favicon.ico"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <meta name="theme-color" content="#000000"/>
-        <meta name="description" content="Web site created using create-react-app"/>
-        <link rel="apple-touch-icon" href="/logo192.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <title>React App</title>
-        <script defer="defer" src="/main.js"></script>
-        <link href="/main.css" rel="stylesheet">
-    </head>
-      <body>
-        <div id="root">${content}</div>
-      </body>
-    </html>
-  `;
-
-  res.send(html);
-});
-
-app.get("/", (req, res, next) => {
-  const content = ReactDOMServer.renderToString(<App />);
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="/favicon.ico"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-        <meta name="theme-color" content="#000000"/>
-        <meta name="description" content="Web site created using create-react-app"/>
-        <link rel="apple-touch-icon" href="/logo192.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <title>React App</title>
-        <script defer="defer" src="/main.js"></script>
-        <link href="/main.css" rel="stylesheet">
-    </head>
-      <body>
-        <div id="root">${content}</div>
-      </body>
-    </html>
-  `;
-
-  res.send(html);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
--- src/AppServer.js
+++ src/AppServer.js
@@ -1,18 +1,3 @@
-// import React, { useState } from 'react';
-
-// const AppServer = () => {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <h1>Hello from Server-Side Rendered React App!</h1>
-//       <p>Counter: {count}</p>
-//       <button onClick={() => setCount(count + 1)}>Increment</button>
-//     </div>
-//   );
-// };
-
-// export default AppServer;
 import React, { useState } from "react";
 import "./App.css";
 
